dbc546fea3d6a7183dbcfbcd837afd558f9dd932
Helen Yau <helenyau13@gmail.com>
2017-12-14 15:04:13 -0800

    fix users profile get route

--- src/controllers/routes/home.js
+++ src/controllers/routes/home.js
@@ -5,7 +5,6 @@ router.get('/', (req, res) => {
   cities.getAll()
   .then(cities => {
     const error = req.query.error
-    console.log(error)
     res.render('index', { cities, error })
   })
 })
