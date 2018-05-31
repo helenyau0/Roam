eb4ef4b068d201c9dae703f1dddfbf3fed611c70
Helen Yau <helenyau13@gmail.com>
2018-05-31 12:02:52 -0700

    fix bug preventing javascript from executing

--- public/index.js
+++ public/index.js
@@ -2,27 +2,19 @@ UPLOADCARE_PUBLIC_KEY="6b5326acd4ea5f65a9c3"
 
 document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector(".carousel-item").classList.add("active");
-  document.querySelector('.editButton').addEventListener('click', openModal)
-  document.querySelector('.close').addEventListener('click', closeModal)
-  document.querySelector('.exit').addEventListener('click', closeModal)
-  document.querySelectorAll('.delete').forEach((deleted => {deleted.addEventListener('click', deletePost)}))
 })
 
-const openModal = () => {
-  document.querySelector('.modal').style.display = 'block'
-}
+window.onload = function(event) {
+  document.querySelectorAll('.delete').forEach((deleted => {deleted.addEventListener('click', deletePost)}))
 
-const closeModal = () => {
-  document.querySelector('.modal').style.display = 'none'
-}
-
-const deletePost = (event) => {
-  const post = JSON.parse(event.target.value)
-  if (confirm(`Are you sure you want to delete "${post.title}"?`)) {
-   fetch(`/posts/delete/${post.id}`, {
-      method: 'post'
-    }).then(success => {      
-      event.target.parentNode.parentNode.parentNode.remove()
-    }).catch(console.log)
+  function deletePost(event) {
+    const post = JSON.parse(event.target.value)
+    if (confirm(`Are you sure you want to delete "${post.title}"?`)) {
+    fetch(`/posts/delete/${post.id}`, {
+        method: 'post'
+      }).then(success => {      
+        event.target.parentNode.remove()
+      }).catch(console.log)
+    }
   }
 }
