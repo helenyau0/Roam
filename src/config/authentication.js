49625d8f4cec47d149a7c991fb443f79e8bcadb9
helen <>
2019-04-01 12:50:49 -0700

    update dependencies and fix pass check

--- src/config/authentication.js
+++ src/config/authentication.js
@@ -1,62 +1,73 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const bcrypt = require('bcrypt')
-const dbUsers = require('../models/users')
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const dbUsers = require('../models/users');
 
-const encryptPassword = (password) =>
-  bcrypt.hashSync(password, 10)
+const encryptPassword = password => bcrypt.hashSync(password, 10);
 
-passport.use(new LocalStrategy(
-  (email, password, done) => {
-    dbUsers.findByEmail(email)
-    .then(foundUser => {
-      if (bcrypt.compareSync(password, foundUser.password)) {
-         return done(null, foundUser)
-      } else {
-        return done(null, false, { message: "Invalid password" })
-      }
-    }).catch(noUserFound => {
-      return done(null, false, { message: "Invalid email" })
-    })
+passport.use(
+  new LocalStrategy((email, password, done) => {
+    dbUsers
+      .findByEmail(email)
+      .then(foundUser => {
+        if (bcrypt.compareSync(password, foundUser.password)) {
+          return done(null, foundUser);
+        } else {
+          return done(null, false, { message: 'Invalid password' });
+        }
+      })
+      .catch(noUserFound => {
+        return done(null, false, { message: 'Invalid email' });
+      });
   })
-)
+);
 
-passport.use('signup', new LocalStrategy({
-  usernameField : 'email',
-  passwordField : 'password',
-  passReqToCallback: true
-}, (req, email, password, done) => {
-  if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password)) {
-    return done(null, false, { message: "Password must be longer than 8 and contain at least: 1 capital letter, 1 lowercase letter, and 1 number"})
-  }
-  if(password !== req.body.confirm) {
-    return done(null, false, { message: "Passwords do not match, try again."})
-  } else {
-    dbUsers.findByEmail(email)
-    .then(user => {
-      if(user) {
-        return done(null, false, { message: "This email is already taken." })
-      } else if(user === null) {
-        const hash = encryptPassword(password)
-        dbUsers.create(req.body, hash)
-        .then(user => {
-          return done(null, user)
-        })
+passport.use(
+  'signup',
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    (req, email, password, done) => {
+      if (password.length < 8) {
+        return done(null, false, {
+          message:
+            'Password must be longer than 8 and contain at least: 1 capital letter, 1 lowercase letter, and 1 number'
+        });
       }
-    })
-  }
-}))
+      if (password !== req.body.confirm) {
+        return done(null, false, {
+          message: 'Passwords do not match, try again.'
+        });
+      } else {
+        dbUsers.findByEmail(email).then(user => {
+          if (user) {
+            return done(null, false, {
+              message: 'This email is already taken.'
+            });
+          } else if (user === null) {
+            const hash = encryptPassword(password);
+            dbUsers.create(req.body, hash).then(user => {
+              return done(null, user);
+            });
+          }
+        });
+      }
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
+  done(null, user.id);
+});
 
 passport.deserializeUser((id, done) => {
-  dbUsers.findById(id)
-  .then(user => done(null, user))
-})
+  dbUsers.findById(id).then(user => done(null, user));
+});
 
-module.exports =  {
+module.exports = {
   encryptPassword,
   passport
-}
+};
